082fa9a412ff82ab3b4802d3b3d63d351429d895
wangtaogh <46988971+wangtaogh@users.noreply.github.com>
2022-05-19 09:13:40 +0800

    Update login.js
    不包含空格++

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ git config --global http.sslVerify "false"
 
 
 //推送到远程仓库时报错，可用
-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
+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 ++
 30天有效期，过后要重新生成
 
 git remote set-url origin https://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@github.com/wangtaogh/proj_02.git
