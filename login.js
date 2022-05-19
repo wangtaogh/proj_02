2c8eafd8a48abdd5861a9ccedc6adc0bd8e68176
wangtaogh <wangtao030156@163.com>
2022-05-19 09:10:34 +0800

    login.js中增加常用的git错误和解决

--- login.js
+++ login.js
@@ -1,1 +1,17 @@
-console.log("hello git")
+console.log("hello git")；
+console.log("hello git")；
+//git clone https://... 报错时可用此命令
+git config --global http.sslVerify "false"
+
+
+//推送到远程仓库时报错，可用
+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
+30天有效期，过后要重新生成
+
+git remote set-url origin https://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@github.com/wangtaogh/proj_02.git
+git push
+
+
+//从远程仓库拉取最新代码时
+
+
